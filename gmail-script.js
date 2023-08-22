fb45069c04ee482e4ee75ad74c760d9903cf3189
Chris Woodlief <cwoodlief6@gmail.com>
2023-08-22 00:12:08 -0400

    gmail script to work outside of inbox

--- gmail-script.js
+++ gmail-script.js
@@ -2,74 +2,98 @@
 window.onload = function () {
   console.log("Window loaded.");
 
-  // Function to execute when the URL hash changes (e.g., navigating within Gmail)
-  window.onhashchange = () => {
-    console.log("URL hash changed.");
+  // Call once on initial page load
+  setupReplyButtonListenerIfNeeded();
 
-    // Check if the hash corresponds to an inbox URL
-    if (window.location.hash.startsWith("#inbox/")) {
-      console.log("Inbox URL detected.");
-
-      const spans = document.querySelectorAll("span");
-      // Iterate through all span elements on the page
-      for (const span of spans) {
-        if (span.innerText === "Reply") {
-          console.log("Reply button found.");
-
-          // Add a click event listener to the Reply button
-          span.addEventListener("click", function () {
-            console.log("Reply button clicked.");
-
-            const email = document.querySelector(".adn.ads");
-            if (email) {
-              const emailContent = email.textContent.replace(/\n/g, " ");
-              console.log("Email content retrieved.");
-
-              // Format the email content for GPT's understanding
-              const formattedEmailContent =
-                "Respond to the most recent email in a comprehensive and professional tone and sign off with my name (Michael Flint) at the end: \n" +
-                emailContent;
-              console.log("Email content formatted for GPT.");
-
-              const loadingIndicationInterval = setInterval(() => {
-                const numDots = countDotsInString(getGmailTextboxText());
-                const newDotsCount = numDots < 5 ? numDots + 1 : 0;
-                updateGmailTextboxText(
-                  `Loading${getStringWithNumDots(newDotsCount)}`
-                );
-              }, 200);
-
-              chrome.runtime.sendMessage(
-                { type: "gptRequest", emailContent: formattedEmailContent },
-                function (response) {
-                  clearInterval(loadingIndicationInterval);
-                  if (response.error) {
-                    console.error(response.error);
-                    return;
-                  }
-
-                  updateGmailTextboxText(response.response);
-                  console.log("Gmail textbox updated with GPT response.");
-
-                  // Send a message to the background script with the status update
-                  chrome.runtime.sendMessage({
-                    type: "updateStatus",
-                    extensionStatus: "Active",
-                    lastEmail: emailContent,
-                    lastResponse: response.response,
-                  });
-                  console.log("Status message sent to background script.");
-                }
-              );
-            } else {
-              console.error("Email content not found.");
-            }
-          });
-        }
-      }
+  const inboxCheckInterval = setInterval(() => {
+    if (doesInboxAppearOnPage()) {
+      clearInterval(inboxCheckInterval);
+      setupReplyButtonListenerIfNeeded();
     }
+  }, 50);
+
+  setTimeout(() => {
+    clearInterval(inboxCheckInterval);
+  }, 4000);
+
+  // Call again when a hash changes
+  window.onhashchange = () => {
+    setupReplyButtonListenerIfNeeded();
   };
+
+  window.addEventListener("popstate", function (event) {
+    setupReplyButtonListenerIfNeeded();
+  });
 };
+
+function doesInboxAppearOnPage() {
+  const spans = document.querySelectorAll("span");
+  for (const span of spans) {
+    if (span.innerText === "Reply") {
+      return true;
+    }
+  }
+}
+
+function setupReplyButtonListenerIfNeeded() {
+  const spans = document.querySelectorAll("span");
+  // Iterate through all span elements on the page
+  for (const span of spans) {
+    if (span.innerText === "Reply") {
+      console.log("Reply button found.");
+
+      // Add a click event listener to the Reply button
+      span.addEventListener("click", function () {
+        console.log("Reply button clicked.");
+
+        const email = document.querySelector(".adn.ads");
+        if (email) {
+          const emailContent = email.textContent.replace(/\n/g, " ");
+          console.log("Email content retrieved.");
+
+          // Format the email content for GPT's understanding
+          const formattedEmailContent =
+            "Respond to the most recent email in a comprehensive and professional tone and sign off with my name (Michael Flint) at the end: \n" +
+            emailContent;
+          console.log("Email content formatted for GPT.");
+
+          const loadingIndicationInterval = setInterval(() => {
+            const numDots = countDotsInString(getGmailTextboxText());
+            const newDotsCount = numDots < 5 ? numDots + 1 : 0;
+            updateGmailTextboxText(
+              `Loading${getStringWithNumDots(newDotsCount)}`
+            );
+          }, 200);
+
+          chrome.runtime.sendMessage(
+            { type: "gptRequest", emailContent: formattedEmailContent },
+            function (response) {
+              clearInterval(loadingIndicationInterval);
+              if (response.error) {
+                console.error(response.error);
+                return;
+              }
+
+              updateGmailTextboxText(response.response);
+              console.log("Gmail textbox updated with GPT response.");
+
+              // Send a message to the background script with the status update
+              chrome.runtime.sendMessage({
+                type: "updateStatus",
+                extensionStatus: "Active",
+                lastEmail: emailContent,
+                lastResponse: response.response,
+              });
+              console.log("Status message sent to background script.");
+            }
+          );
+        } else {
+          console.error("Email content not found.");
+        }
+      });
+    }
+  }
+}
 
 function getStringWithNumDots(n) {
   return ".".repeat(n);
